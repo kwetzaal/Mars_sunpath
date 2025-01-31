e4a50ec0e82612e73f264db7a7ca31912f4acfd1
kwetzaal <kirazhong@gmail.com>
2025-01-31 16:58:48 -0500

    Add files via upload

--- Sunpath Calculations.py
+++ Sunpath Calculations.py
@@ -5,14 +5,19 @@ def solar_altitude_calculations(Ls, time, lat):
     solar_alt = []
     i = 0
     while i < len(time):
-        #axial_tilt = np.deg2rad(24.936) #24.936 is axial tilt
+        hour_angle = np.deg2rad(15*time[i]-180) #time in martian hour (1/24 sol)
 
-        declination = np.arcsin(0.42565*np.sin(Ls))*0.25*np.sin(Ls)
-        hour_angle = np.deg2rad(15*(time[i]-12)) #time in martian hour (1/24 sol)
+        declination = np.arcsin(0.42565*np.sin(Ls))+0.25*(np.sin(Ls)) # From JPL
 
-        #solar_altitude = np.arcsin(np.sin(np.deg2rad(lat))*np.sin(declination)+np.cos(np.deg2rad(lat))*np.cos(declination)*np.cos(np.deg2rad(hour_angle)))
-        solar_zenith = np.arccos(np.sin(declination)*np.cos(lat)+np.cos(declination)**np.sin(lat)*np.cos(hour_angle))
-        solar_altitude = 90-np.rad2deg(solar_zenith)
+        #declination = np.deg2rad(25.2 * np.sin(np.deg2rad((360*(day-1))/687))) # for day not Ls
+
+        # From Earth models
+        solar_altitude = np.rad2deg(np.arcsin(np.sin(lat)*np.sin(declination)+np.cos(lat)*np.cos(declination)*np.cos(hour_angle)))
+
+        # from NASA JPL
+        #solar_zenith = np.arccos(np.sin(declination)*np.sin(lat)+np.cos(declination)*np.cos(lat)*np.cos(hour_angle))
+        #solar_altitude = 90 - np.rad2deg(solar_zenith)
+
         solar_alt.append(solar_altitude)
         i += 1
     return solar_alt
@@ -21,49 +26,91 @@ def solar_azimuth_calculations(Ls, time, lat, solar_altitude):
     solar_azi = []
     i = 0
     while i < len(time):
+        hour_angle = np.deg2rad(15*time[i]-180) #time in martian hour (1/24 sol)
 
-        declination = np.arcsin(0.42565*np.sin(Ls))*0.25*np.sin(Ls)
-        hour_angle = np.deg2rad(15*(time[i]-12)) #time in martian hour (1/24 sol)
-        
-        #solar_azimuth = np.arccos(np.cos(declination)*np.sin(np.deg2rad(hour_angle))/np.cos(np.deg2rad(solar_altitude[i])))
+        declination = np.arcsin(0.42565*np.sin(Ls))+0.25*(np.sin(Ls)) # from JPL
 
-        # equation 6
-        #solar_azimuth = np.arccos(np.sin(np.deg2rad(lat))*np.cos(np.deg2rad(90-solar_altitude[i]))-np.sin(declination))/(np.cos(np.deg2rad(lat))*np.sin(np.deg2rad(90-solar_altitude[1])))
+        #declination = np.deg2rad(25.2 * np.sin(np.deg2rad((360*(day-1))/687))) # for day not Ls
 
-        solar_azimuth = np.arctan(np.sin(hour_angle)/(np.cos(lat)*np.tan(declination)-np.sin(lat)*np.cos(hour_angle)))
-        solar_azi.append(np.rad2deg(solar_azimuth)+180)
+        # From Earth models
+        solar_azimuth = np.rad2deg(np.arcsin(np.cos(declination)*np.sin(hour_angle)/np.cos(np.deg2rad(solar_altitude[i]))))
 
+        # from NASA JPL
+        #solar_azimuth = np.arctan((np.sin(hour_angle))/(np.cos(lat)*np.tan(declination)-np.sin(lat)*np.cos(hour_angle)))
+        #solar_azi.append(np.rad2deg(solar_azimuth))
+
+        solar_azi.append(solar_azimuth)
         i += 1
     return solar_azi
 
 def main():
-    #Ls = int(input("Solar Longitude: "))
-    #n = int(input("nth orbit of Mars since Aug 26, 1996 (Vernal Equinox): "))
-    #Ls = [0, 30, 60, 90, 120, 150, 180, 210, 240, 270, 300, 330, 360]
+    #intitialize values
+    sol_long = input("Input solar longitude (deg): ")
+    Ls = np.deg2rad(sol_long)
 
-    Ls = np.deg2rad(0)
-    full_time = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]
+    # choice of time values
+    full_time = np.arange(0, 24, 0.5)
     day_time = np.arange(6, 18, 0.1)
-    time = day_time
-    lat = np.deg2rad(40) #0 Ls and 0 lat breaks the code and IDK how to fix it :(
-    if lat == 0:
-        lat = 0.1e-3
+    select = False
+    while select == False:
+        time_choice = input("Please select Full day (F) or Half day(H):")
+        if time_choice == "F":
+            time = full_time
+            select = True
+        elif time_choice == "H":
+            time = day_time
+            select = True
+        else:
+            print("Please select a valid option (F/H)")
 
+    # calculations for different latitudes
+    solar_altitude_0 = solar_altitude_calculations(Ls, time, np.deg2rad(1e-3))
+    solar_azimuth_0 = solar_azimuth_calculations(Ls, time, np.deg2rad(1e-3), solar_altitude_0)
 
-    solar_altitude = solar_altitude_calculations(Ls, time, lat)
-    solar_azimuth = solar_azimuth_calculations(Ls, time, lat, solar_altitude)
+    solar_altitude_15 = solar_altitude_calculations(Ls, time, np.deg2rad(15))
+    solar_azimuth_15 = solar_azimuth_calculations(Ls, time, np.deg2rad(15), solar_altitude_15)
 
-    print(solar_altitude)
-    print(solar_azimuth)
+    solar_altitude_30 = solar_altitude_calculations(Ls, time, np.deg2rad(30))
+    solar_azimuth_30 = solar_azimuth_calculations(Ls, time, np.deg2rad(30), solar_altitude_30)
 
-    x_scale = [0, 30, 60, 90, 120, 150, 180, 210, 240, 270, 300, 330, 360]
-    y_scale = [0, 30, 60, 90, 180, 210, 240, 270, 300, 330, 360]
-    plt.plot(solar_azimuth, solar_altitude)
-    # plt.plot(solar_azimuth, solar_altitude)
-    plt.xlabel("Solar Azimuth(deg)")
-    #plt.xticks(x_scale)
-    plt.ylabel("Solar Altitude(deg)")
-    plt.grid()
+    solar_altitude_60 = solar_altitude_calculations(Ls, time, np.deg2rad(60))
+    solar_azimuth_60 = solar_azimuth_calculations(Ls, time, np.deg2rad(60), solar_altitude_60)
+
+    solar_altitude_90 = solar_altitude_calculations(Ls, time, np.deg2rad(90))
+    solar_azimuth_90 = solar_azimuth_calculations(Ls, time, np.deg2rad(90), solar_altitude_90)
+
+    # plots all altitudes and azimuths
+        # plot for solar altitude vs. time
+    fig, axs = plt.subplots(2, 2)
+    axs[0, 0].plot(time, solar_altitude_0, label = "lat = 0")
+    axs[0, 0].plot(time, solar_altitude_15, label = "lat = 15")
+    axs[0, 0].plot(time, solar_altitude_30, label = "lat = 30")
+    axs[0, 0].plot(time, solar_altitude_60, label = "lat = 60")
+    axs[0, 0].plot(time, solar_altitude_90, label = "lat = 90")
+    axs[0, 0].set_title("Solar Altitude vs. Time")
+    axs[0, 0].grid()
+    axs[0, 0].legend()
+
+        # plot for solar azimuth vs. time
+    axs[0, 1].plot(time, solar_azimuth_0, label = "lat = 0")
+    axs[0, 1].plot(time, solar_azimuth_15, label = "lat = 15")
+    axs[0, 1].plot(time, solar_azimuth_30, label = "lat = 30")
+    axs[0, 1].plot(time, solar_azimuth_60, label = "lat = 60")
+    axs[0, 1].plot(time, solar_azimuth_90, label = "lat = 90")
+    axs[0, 1].set_title("Solar Azimuth vs. Time")
+    axs[0, 1].grid()
+    axs[0, 1].legend()
+
+        # plot for solar altitude vs. solar azimuth
+    axs[1, 0].plot(solar_azimuth_0, solar_altitude_0, label = "lat = 0")
+    axs[1, 0].plot(solar_azimuth_15, solar_altitude_15, label = "lat = 15")
+    axs[1, 0].plot(solar_azimuth_30, solar_altitude_30, label = "lat = 30")
+    axs[1, 0].plot(solar_azimuth_60, solar_altitude_60, label = "lat = 60")
+    axs[1, 0].plot(solar_azimuth_90, solar_altitude_90, label = "lat = 90")
+    axs[1, 0].set_title("Solar Azimuth vs. Solar Altitude")
+    axs[1, 0].grid()
+    axs[1, 0].legend()
+
     plt.show()
 
 if __name__ == "__main__":
